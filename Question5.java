432a56cac48ef06195eea3f7339cad453fa5f8be
Ho-Jun-An <74398396+Ho-Jun-An@users.noreply.github.com>
2021-04-25 17:15:02 +0800

    Completed Question 5

--- Question5.java
+++ Question5.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Question5
 {
@@ -26,7 +27,38 @@ public class Question5
      * Hint: Use a loop to get input. Use another 2 loops to find the mode
      */
      
-    Scanner in = new Scanner(System.in);
-    
+    Scanner amtObj = new Scanner(System.in);
+    System.out.println("Enter number of integers you will enter:");
+    int amt = amtObj.nextInt();
+
+    ArrayList<Integer> numList = new ArrayList<>();
+
+    for(int i = amt; i > 0; i--){
+      Scanner numObj = new Scanner(System.in);
+      System.out.println("Enter number:");
+      int num = numObj.nextInt();
+
+      numList.add(num);
+    }
+
+    int popular = numList.get(0);
+    int popularCount = 0;
+
+    for(int element : numList){
+      int count = 0;
+
+      for(int check : numList){
+        if(check == element){
+          count++;
+        }
+      }
+
+      if(count > popularCount){
+        popular = element;
+        popularCount = count;
+      }
+    }
+
+    System.out.println("The mode is: " + popular);
   }
 }
